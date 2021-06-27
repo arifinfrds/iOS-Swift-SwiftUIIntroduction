0ff7bd07897a34e78fa977b30f1f1ebc0da3a9cb
Arifin Firdaus <arifin.firdaus@icehousecorp.com>
2021-06-27 09:53:32 +0700

    Initial Commit

---
+++ Shared/SwiftUIIntroductionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIIntroductionApp.swift
+//  Shared
+//
+//  Created by Arifin Firdaus on 27/06/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIIntroductionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
